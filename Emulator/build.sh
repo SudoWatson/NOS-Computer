937b9c79ac0d4b7f5424311114f55757163d094d
SudoWatson <aulennert@protonmail.com>
2024-11-08 10:47:09 -0600

    CLANGD working

--- Emulator/build.sh
+++ Emulator/build.sh
@@ -9,7 +9,9 @@ fi
 cd build
 
 # Run CMake to configure the project and generate Makefiles
-cmake ..
+# cmake ..
+# TODO: Can I add this as flag in cmake file instead?
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..
 
 # Compile the project using Make
 make
