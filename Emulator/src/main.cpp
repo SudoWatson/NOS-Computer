4f40b6d749ab75946ddab9f0fe6113e17a04a900
SudoWatson <aulennert@protonmail.com>
2025-03-27 22:07:34 -0500

    TUI Start

--- Emulator/src/main.cpp
+++ Emulator/src/main.cpp
@@ -1,35 +1,50 @@
 // src/main.cpp
 #include "CPU.h"
 #include <cstdint>
+#include <ncurses.h>
 #include <iostream>
 #include <ostream>
 
-#define log(x) std::cout << x << std::endl
-
-class valueHolder {
-public:
-    valueHolder(uint16_t _value=0) {value = _value;}
-    uint16_t value;
-};
-
 #define runInstruction() cpu.FullCycle();cpu.FullCycle();cpu.FullCycle();cpu.FullCycle();cpu.FullCycle();cpu.FullCycle();cpu.FullCycle();
 
-int main() {
+WINDOW* createWindow(int x, int y, int width, int height, const char* title)
+{
+    WINDOW* win = newwin(width, height, x, y);
+    box(win, 0, 0);
+    mvwprintw(win, 0, 1, title);
+    wrefresh(win);
+    return win;
+}
 
-    CPU cpu;
-    /*
-     * Figuring out the instructions
-     * For the immediate load, I need to increment the Program Counter a second time somewhere in the instruction set
-     * hopefully without making an entire step for it
-        *
-        */
-
-    // Setup
-    runInstruction();
-    runInstruction();
-
-    for (int i = 0; i < 1000; i++)
+void updateRegisterWindow(WINDOW* win, CPU& cpu)
+{
+    werase(win);
+    box(win, 0, 0);
+    mvwprintw(win, 3, 3, "Test");
+    for (int i = 0; i < 16; i++)
     {
+        mvwprintw(win, 1 + (i / 2), (i % 2) * 15 + 2, "R%d: 0x%04X", i, cpu.rc->generalRegisters[i]->readValue());
+    }
+    wrefresh(win);
+}
+
+int main() {
+    CPU cpu;
+
+
+    initscr();
+    refresh();
+
+    WINDOW* registers = createWindow(0, 0, 200, 50, "Registers");
+
+
+    runInstruction();
+    runInstruction();
+
+    for (int i = 0; i < 100; i++)
+    {
+        updateRegisterWindow(registers, cpu);
+        getch();  // Get character, waits for input
         // Adds
         runInstruction();
         runInstruction();
@@ -38,7 +53,13 @@ int main() {
         runInstruction();
     }
 
-
+    endwin();
     return 0;
 }
+
+
+
+
+
+
 
